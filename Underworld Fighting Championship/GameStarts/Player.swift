715ee595bd7b04f57306ef23fd0ad03956460aa6
Roland Lariotte <roland.sound@live.fr>
2019-03-26 15:39:40 +0100

    Update to Swift 5.

--- Underworld Fighting Championship/GameStarts/Player.swift
+++ Underworld Fighting Championship/GameStarts/Player.swift
@@ -9,31 +9,32 @@
 import Foundation
 
 internal class Player {
-
+  
   var listOfFighters = [Fighter]()
-
+  
   // For the player to make his team and name each player chosen
   func makeTheTeam(listOfFightersName: [String]) -> [String] {
-
+    
     var listOfFightersName = listOfFightersName
-
+    
     // Present all fighters available
     gameAction.charactersPresentation()
-
+    
     // Loop for players to select and name their fighters
     while listOfFighters.count < 3 {
       print("\nInput an associated number to select a fighter:")
+      
       if let userChoice = readLine() {
-
         print("\nName your fighter:")
+        
         if let nameChoice = readLine() {
-
+          
           if listOfFightersName.contains(nameChoice) {
             print("\nName your fighter uniquely!")
           }
           else {
             listOfFightersName.append(nameChoice)
-
+            
             switch userChoice {
             case "1":
               listOfFighters.append(Warrior(fighterName: "\(nameChoice.capitalized)"))
@@ -53,20 +54,20 @@ internal class Player {
     showPlayerChoice()
     return listOfFightersName
   }
-
+  
   // To show the player' s choice after he made it
   func showPlayerChoice() {
-
+    
     print("\n\nYour chosen fighters are: \n")
     for fighter in listOfFighters {
       print("\(fighter) \(fighter.fighterName)")
     }
     print("")
   }
-
+  
   // To describe player' s fighters names as well as their life points left and attack points
   func fightersDescription() {
-
+    
     print("""
       
       1: \(listOfFighters[0])  \(listOfFighters[0].fighterName),  \(listOfFighters[0].lifePoints) life points,  \(listOfFighters[0].hittingScore()) attack points.
@@ -75,15 +76,15 @@ internal class Player {
       
       """)
   }
-
+  
   // To use when needed to choose between player' s fighters list during the fight loop
   func fightersSwitch() -> Fighter {
-
+    
     var playerChosen: Fighter?
-
+    
     while playerChosen == nil {
       if let userChoice = readLine() {
-
+        
         switch userChoice {
         case "1":
           playerChosen = listOfFighters[0]
@@ -94,7 +95,7 @@ internal class Player {
         default:
           print("\n!!! Enter a number associated to a fighter to continue !!!\n")
         }
-
+        
         if let player = playerChosen, player.lifePoints <= 0 {
           print("\n\n☠️ A DEAD FIGHTER IS A DEAD MAN ☠️\n\nChoose one still alive to continue...")
           playerChosen = nil
@@ -103,11 +104,11 @@ internal class Player {
     }
     return playerChosen!
   }
-
+  
   // Boolean that checks if a fighter is still alive
   func haveFightersAlive() -> Bool {
     for fighter in listOfFighters {
-
+      
       if fighter.lifePoints > 0 {
         return true
       }
